e644e930c0fcb1d96d4dc6beb185ef350ed98526
dzonsi <nikola86_v@yahoo.com>
2020-06-23 18:32:38 +0200

    Add connected SideNav and SideNavToggle to Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavContainer } from './NavContainer';
 import { NavContent } from './NavContent';
-import { SideNavToggleStyled as SideNavToggle } from './SideNavToggle';
+import { SideNavToggleConnected as SideNavToggle } from './SideNavToggle';
 import { SearchStyled as Search } from './search/Search';
 import { UserInfoConnected as UserInfo } from './user/UserInfo';
-import { SideNavStyled as SideNav } from './side-nav/SideNav';
+import { SideNavConnected as SideNav } from './side-nav/SideNav';
 import { OverlayStyled as Overlay } from './Overlay';
 
 // action from redux
