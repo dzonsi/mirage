e162bbeab209cf21b3e33fdd54b6082aa517f145
dzonsi <nikola86_v@yahoo.com>
2020-07-01 14:22:23 +0200

    Add usersReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import navbarReducer from './navbarReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-	navbarReducer
+	navbarReducer,
+	usersReducer
 });
