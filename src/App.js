a4b7c20e404dc0f276d801f527af0c303d55090f
dzonsi <nikola86_v@yahoo.com>
2020-07-01 14:34:32 +0200

    Add Users component to App component

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import { AppContainer } from './components/AppContainer';
 import { ErrorContainer } from './components/ErrorContainer';
 import Navbar from './components/navbar/Navbar';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import Users from './components/Users';
 
 // add icons to library for use anywhere in app
 library.add(fab, fas, far);
@@ -45,6 +46,7 @@ function App() {
       <GlobalStyles />
       <AppContainer>
         <Navbar />
+        <Users />
         <h2>{ theme === 'light' ? 'Light theme' : 'Dark Theme'}</h2>
         <button onClick={toggleTheme}>Toggle theme</button>
         <Router>
