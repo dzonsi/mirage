fcc78724176a99e33852a7aeeb13b859394cb270
dzonsi <nikola86_v@yahoo.com>
2020-07-01 14:33:14 +0200

    Add Users.js component to test data fetching with Redux

---
+++ src/components/Users.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from '../action-creators/actionCreators';
+
+class Users extends React.Component {
+
+	componentDidMount() {
+		this.props.dispatch(fetchUsers());
+	}
+
+	render() {
+		const { loading, users, error } = this.props;
+		if(loading) {
+			return <h2>Loading...</h2>
+		}
+		if(users.length) {
+			return (
+				<ul>
+					{users.map(user => <li key={user.id}>{user.name}</li>)}
+				</ul>
+			)
+		}
+		if(error) {
+			return (
+				<h2>Something went wrong!</h2>
+			)
+		}
+		return (
+			<h2>Users component</h2>
+		)
+	}
+
+}
+
+const mapStateToProps = state => ({
+	users: state.usersReducer.users,
+	loading: state.usersReducer.loading,
+	error: state.usersReducer.error
+});
+
+export default connect(mapStateToProps) (Users);
