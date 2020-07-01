7072a31d58d34d7fb45fab67c5a1f06fbc2273b3
dzonsi <nikola86_v@yahoo.com>
2020-07-01 13:03:26 +0200

    Add action creators for users

--- src/action-creators/actionCreators.js
+++ src/action-creators/actionCreators.js
@@ -1,5 +1,10 @@
 import { TOGGLE_USER_INFO } from '../actions/actionTypes';
 import { TOGGLE_SIDE_NAV } from '../actions/actionTypes';
+import { FETCH_USERS_BEGIN } from '../actions/actionTypes';
+import { FETCH_USERS_SUCCESS } from '../actions/actionTypes';
+import { FETCH_USERS_ERROR } from '../actions/actionTypes';
+
+// navbar
 
 export function toggleUserInfo() {
 	return {
@@ -12,3 +17,19 @@ export function toggleSideNav() {
 		type: TOGGLE_SIDE_NAV
 	}
 }
+
+// users
+
+export const fetchUsersBegin = () => ({
+	type: FETCH_USERS_BEGIN
+});
+
+export const fetchUsersSuccess = users => ({
+	type: FETCH_USERS_SUCCESS,
+	payload: { users }
+});
+
+export const fetchUsersError = error => ({
+	type: FETCH_USERS_ERROR,
+	payload: { error }
+});
