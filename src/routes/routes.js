00d7c544bf2e8d8c4767b7ed61a97d5f9fdeabe4
dzonsi <nikola86_v@yahoo.com>
2020-07-02 12:20:45 +0200

    Change route with path "/" to render Home component

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,9 +4,11 @@ import {
   Route,
 } from 'react-router-dom';
 
+import { HomeStyled as Home } from '../components/home/Home';
+
 const routes = (
 	<Switch>
-    <Route exact path="/" render={() => (<h2>Home</h2>)}/>
+    <Route exact path="/" render={() => <Home />}/>
     <Route path="/users" render={() => (<h2>Users</h2>)}></Route>
     <Route path="*" render={() => (<h2>No Match</h2>)}></Route>
   </Switch>
