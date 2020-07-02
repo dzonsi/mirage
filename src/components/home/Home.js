067bb0ec8c34ce1a278d890e822a35b6d16f3d62
dzonsi <nikola86_v@yahoo.com>
2020-07-02 11:56:47 +0200

    Add home folder in components, and Home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+//import { minWidth } from '../../../theme/mixins/minWidth';
+
+
+function Home(props) {
+	return (
+		<h2>Home component</h2>
+	)
+}
+
+export const HomeStyled = styled(Home)`
+	
+`
