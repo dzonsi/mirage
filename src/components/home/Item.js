f00143c63cbe9c8eebe8a9c11b43476578376c63
dzonsi <nikola86_v@yahoo.com>
2020-07-07 15:21:12 +0200

    Add functions to get random image to use as user image

--- src/components/home/Item.js
+++ src/components/home/Item.js
@@ -3,19 +3,22 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+// import all images as object
 function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
 }
+const images = importAll(require.context('../../assets/images/avatar', false, /\.png/));
 
-const images = importAll(require.context('../../assets/images/avatar', false, '/\.png/'));
+// array from all images keys
+const imgArray = Object.keys(images);
 
+// math random between to numbers
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-const num = getRandom(0, images.length);
-console.log(num);
+const num = getRandom(0, imgArray.length - 1);
 
 function Item(props) {
 
@@ -23,7 +26,7 @@ function Item(props) {
 
 	return (
 		<NavLink to={`/users/${user.id}`} className={props.className}>
-			<img src={images[num]} alt="User profile" />
+			<img src={images[imgArray[num]]} alt="User profile" />
 			<h3>{user.name}</h3>
 			<p>{user.address.city}</p>
 		</NavLink>
