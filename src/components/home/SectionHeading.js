8e503305e39a6c939e34e00bff153f2a7d9b0652
dzonsi <nikola86_v@yahoo.com>
2020-07-07 14:01:36 +0200

    Add style to SectionHeading component

--- src/components/home/SectionHeading.js
+++ src/components/home/SectionHeading.js
@@ -15,12 +15,35 @@ function SectionHeading(props) {
 	return (
 		<NavLink to={to} className={props.className}>
 			<h2>{capitalize(to)}</h2>
-			<Icon icon={['fas', 'arrow-right']} />
+			<Icon icon={['fas', 'arrow-right']}  />
 		</NavLink>
 	)
 
 }
 
 export const SectionHeadingStyled = styled(SectionHeading)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: 0 0 1rem;
 
+	&:hover {
+		& h2 {
+			color: ${({ theme }) => theme.zeta};
+		}
+
+		& svg {
+			color: ${({ theme }) => theme.zeta};
+		}
+	}
+
+	& h2 {
+		font-size: 1.4rem;
+		color: ${({ theme }) => theme.alpha};
+		margin: 0;
+	}
+
+	& svg {
+		color: ${({ theme }) => theme.beta};
+	}
 `
