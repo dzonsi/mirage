5708df47ec7992e477d123e395be67228c2c4aa3
dzonsi <nikola86_v@yahoo.com>
2020-07-10 14:49:28 +0200

    Import AllUsers component and show it when route path is /users

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,13 @@ import {
 } from 'react-router-dom';
 
 import { HomeStyled as Home } from '../components/home/Home';
+import { AllUsersConnected as Users } from '../components/users/AllUsers';
 
 const routes = (
 	<Switch>
     <Route exact path="/" render={() => <Home />}/>
     <Route path="/users/:id" render={() => (<h2>User with id...</h2>)}></Route>
-    <Route path="/users" render={() => (<h2>Users</h2>)}></Route>
+    <Route path="/users" render={() => <Users />}></Route>
     <Route path="*" render={() => (<h2>No Match</h2>)}></Route>
   </Switch>
 );
