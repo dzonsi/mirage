5dd12d000c5e2f8e1bc0cc94c30b489955c24ba8
dzonsi <nikola86_v@yahoo.com>
2020-07-10 19:15:34 +0200

    Fetch users if props.users array is empty and show all users

--- src/components/users/AllUsers.js
+++ src/components/users/AllUsers.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { fetchUsers } from '../../action-creators/usersCreators';
 
 import { LoadingStyled as Loading } from '../home/Loading';
 import { ItemsContainerStyled as ItemsContainer } from '../home/ItemsContainer';
 import { ItemStyled as Item } from '../home/Item';
 
+
 function AllUsers(props) {
 
 	const { loading, users, error } = props;
+
+	useEffect(() => {
+		if(!props.users.length) {
+			props.dispatch(fetchUsers());
+		}
+	}, []);
 
 	if(loading) {
 		return (
@@ -17,15 +25,17 @@ function AllUsers(props) {
 	if(users.length) {
 
 		const items = users.map((user, index) => {
-			// return (
-			// 	<Item user={user} key={user.id}/>
-			// );
-			return false;
+			return (
+				<Item user={user} key={user.id} show="all" />
+			);
 		});
 
 		return (
 			<section>
 				<h2>All users!</h2>
+				<ItemsContainer show="all">
+					{items}
+				</ItemsContainer>
 			</section>
 		)
 	}
@@ -34,6 +44,7 @@ function AllUsers(props) {
 			<h2>Something went wrong while fetching users!</h2>
 		)
 	}
+
 	return (
 		<h2>Users</h2>
 	)
