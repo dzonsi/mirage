46f0de1d84c92caf6501b32529d34fb8c214a120
dzonsi <nikola86_v@yahoo.com>
2020-08-27 12:02:32 +0200

    Add action types for sorting users

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,6 +3,14 @@ export const TOGGLE_USER_INFO = 'TOGGLE_USER_INFO';
 export const TOGGLE_SIDE_NAV = 'TOGGLE_SIDE_NAV';
 
 // users
+
+// fetch users
 export const FETCH_USERS_BEGIN = 'FETCH_USERS_BEGIN';
 export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
+
+// sort users
+export const SORT_LOADING = 'SORT_LOADING';
+export const SORT_ID = 'SORT_ID';
+export const SORT_ASC = 'SORT_ASC';
+export const SORT_DESC = 'SORT_DESC';
