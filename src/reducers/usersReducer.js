84d70611598947287fb43c1a7ae34e0be12f40a0
dzonsi <nikola86_v@yahoo.com>
2020-08-27 12:26:19 +0200

    Add cases for sorting users

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,16 @@
 import { FETCH_USERS_BEGIN } from '../actions/actionTypes';
 import { FETCH_USERS_SUCCESS } from '../actions/actionTypes';
 import { FETCH_USERS_ERROR } from '../actions/actionTypes';
+import { SORT_LOADING } from '../actions/actionTypes';
+import { SORT_ID } from '../actions/actionTypes';
+import { SORT_ASC } from '../actions/actionTypes';
+import { SORT_DESC } from '../actions/actionTypes';
 
 const initialState = {
 	users: [],
 	loading: false,
-	error: null
+	error: null,
+	order: 'id'
 };
 
 export default function usersReducer(state = initialState, action) {
@@ -28,6 +33,35 @@ export default function usersReducer(state = initialState, action) {
 				loading: false,
 				error: action.payload,
 				users: []
+			}
+		case SORT_LOADING:
+			return {
+				...state,
+				loading: true
+			}
+		case SORT_ID:
+			const id = [...state.users];
+			const idSorted = id.sort((a, b) => (a.id > b.id) ? 1 : -1);
+			return {
+				...state,
+				users: idSorted,
+				loading: false
+			}
+		case SORT_ASC:
+			const asc = [...state.users];
+			const ascSorted = asc.sort((a, b) => (a.name > b.name) ? 1 : -1);
+			return {
+				...state,
+				users: ascSorted,
+				loading: false
+			}
+		case SORT_DESC:
+			const desc = [...state.users];
+			const descSorted = desc.sort((a, b) => (a.name < b.name) ? 1 : -1);
+			return {
+				...state,
+				users: descSorted,
+				loading: false
 			}
 		default:
 			return state;
