ecac452dcde940ca8f8ae77f51bb725505ec1fbc
dzonsi <nikola86_v@yahoo.com>
2020-08-28 14:06:33 +0200

    Remove px in padding value

--- src/components/shared/DefaultButton.js
+++ src/components/shared/DefaultButton.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const DefaultButton = styled.button`
 	color: inherit;
 	background-color: transparent;
-	padding: ${({ padding }) => padding ? `${padding}px` : 0};
+	padding: ${({ padding }) => padding ? `${padding}` : 0};
 	border: none;
 
 	&:active {
-		padding: ${({ padding }) => padding ? `${padding}px` : 0};
+		padding: ${({ padding }) => padding ? `${padding}` : 0};
 	}
 `
