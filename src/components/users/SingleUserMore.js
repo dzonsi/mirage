81f4d5cd57fcd1d35e5c3a70998d65ee73b08d7d
dzonsi <nikola86_v@yahoo.com>
2020-08-29 15:02:08 +0200

    New component for whole single user options and button that open options

---
+++ src/components/users/SingleUserMore.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from "react";
+import styled from 'styled-components';
+
+import { DefaultButton as Button } from '../shared/DefaultButton';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { SingleUserOptionsStyled as Options } from './SingleUserOptions';
+
+function SingleUserMore(props) {
+
+	const [show, setShow] = useState(false);
+	const btn = useRef(null);
+
+	const getBtn = () => {
+		return btn;
+	}
+
+	return (
+		<div className={props.className}>
+			<Button ref={btn} padding="0 9px" onClick={() => setShow(!show)} title="More options">
+				<Icon icon={['fas', 'ellipsis-v']} />
+			</Button>
+			{show === true ? <Options show={show} setShow={setShow} getBtn={getBtn} /> : null}
+		</div>
+	)
+
+}
+
+export const SingleUserMoreStyled = styled(SingleUserMore)`
+	position: relative;
+`
