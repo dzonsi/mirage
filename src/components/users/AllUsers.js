3488bababb90de4723501b2a1be6cb60357e6bca
dzonsi <nikola86_v@yahoo.com>
2020-09-02 12:28:57 +0200

    Remove unnecessary data from component

--- src/components/users/AllUsers.js
+++ src/components/users/AllUsers.js
@@ -1,33 +1,18 @@
 import React, { useEffect } from "react";
-import { useHistory } from 'react-router-dom';
 import { connect } from "react-redux";
 import styled from 'styled-components';
 
 import { fetchUsers } from '../../action-creators/usersCreators';
-import { sortById } from '../../action-creators/usersCreators';
-import { sortByAsc } from '../../action-creators/usersCreators';
-import { sortByDesc } from '../../action-creators/usersCreators';
 
 import { LoadingStyled as Loading } from '../home/Loading';
 import { ItemsContainerStyled as ItemsContainer } from '../home/ItemsContainer';
 import { ItemStyled as Item } from '../home/Item';
-import { DefaultButton as Button } from '../shared/DefaultButton';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { StatusConnectedStyled as Status } from '../shared/Status';
 import { AllUsersOptionsStyled as Options } from './AllUsersOptions';
 
 function AllUsers(props) {
 
-	const {
-		loading,
-		users,
-		error,
-		sortById,
-		sortByAsc,
-		sortByDesc
-	} = props;
-	const history = useHistory();
-	const goBack = history.goBack;
+	const { loading, users, error } = props;
 
 	useEffect(() => {
 		if(!props.users.length) {
@@ -50,22 +35,9 @@ function AllUsers(props) {
 
 		return (
 			<section className={props.className}>
-				<div>
-					<Button onClick={() => history.goBack()}>
-						<Icon icon={['fas', 'arrow-left']} />
-					</Button>
-					<h2>Users</h2>
-					<button onClick={sortById}>Id</button>
-					<button onClick={sortByAsc}>Asc</button>
-					<button onClick={sortByDesc}>Desc</button>
-					<Button onClick={() => {console.log('Options')}}>
-						<Icon icon={['fas', 'ellipsis-v']} />
-					</Button>
-				</div>
 				<Status
 					type="Users"
 					options={<Options />}
-					goBack={goBack}
 				/>
 				<ItemsContainer show="all">
 					{items}
@@ -92,10 +64,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-	fetchUsers,
-	sortById,
-	sortByAsc,
-	sortByDesc
+	fetchUsers
 }
 
 const AllUsersStyled = styled(AllUsers)`
