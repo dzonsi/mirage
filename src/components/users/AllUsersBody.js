6505cc9b72aa8fd4666ad93325df993df4ba12fb
dzonsi <nikola86_v@yahoo.com>
2020-09-02 13:00:25 +0200

    Add new component AllUsersBody

---
+++ src/components/users/AllUsersBody.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import styled from 'styled-components';
+
+import { fetchUsers } from '../../action-creators/usersCreators';
+
+import { LoadingStyled as Loading } from '../home/Loading';
+import { ItemsContainerStyled as ItemsContainer } from '../home/ItemsContainer';
+import { ItemStyled as Item } from '../home/Item';
+
+function AllUsersBody(props) {
+
+	const { loading, users, error } = props;
+
+	useEffect(() => {
+		if(!props.users.length) {
+			props.fetchUsers();
+		}
+	}, []);
+
+	if(loading) {
+		return (
+			<Loading />
+		)
+	}
+	if(users.length) {
+
+		const items = users.map((user, index) => {
+			return (
+				<Item user={user} key={user.id} show="all" />
+			);
+		});
+
+		return (
+			<section className={props.className}>
+				<ItemsContainer show="all">
+					{items}
+				</ItemsContainer>
+			</section>
+		)
+	}
+	if(error) {
+		return (
+			<h2>Something went wrong while fetching users!</h2>
+		)
+	}
+
+	return (
+		<h2>All Users Body</h2>
+	)
+
+}
+
+const mapStateToProps = state => ({
+	users: state.usersReducer.users,
+	loading: state.usersReducer.loading,
+	error: state.usersReducer.error
+});
+
+const mapDispatchToProps = {
+	fetchUsers
+}
+
+const AllUsersBodyStyled = styled(AllUsersBody)`
+
+`
+
+export const AllUsersBodyConnected = connect(mapStateToProps, mapDispatchToProps) (AllUsersBodyStyled);
