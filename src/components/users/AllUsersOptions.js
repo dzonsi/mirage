ff3d8e5cd07ecd6aa46beab7675bd0214e78f1de
dzonsi <nikola86_v@yahoo.com>
2020-09-05 15:36:33 +0200

    Change style of select tag and made some style changes

--- src/components/users/AllUsersOptions.js
+++ src/components/users/AllUsersOptions.js
@@ -75,7 +75,7 @@ export const AllUsersOptionsStyled = styled(AllUsersOptionsConnected)`
 	height: 200px;
 	text-align: center;
 	color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
-	background-color: ${({ theme }) => theme.name === 'light' ? theme.gamma : theme.beta};
+	background-color: ${({ theme }) => theme.name === 'light' ? theme.delta : theme.beta};
 	border-radius: 20px;
 	animation: ${optionsFadeIn} .2s forwards;
 
@@ -94,7 +94,41 @@ export const AllUsersOptionsStyled = styled(AllUsersOptionsConnected)`
 	}
 
 	& label {
+		font-weight: 700;
+	}
+
+	& select {
+		display: block;
+		width: 80%;
+		max-width: 100%;
+		font-size: 1rem;
+		font-weight: 700;
+		padding: .6rem 1.4rem .5rem .8rem;
+		border-radius: 0.5rem;
 		margin: 0;
+		appearance: none;
+		background-color: ${({ theme }) => theme.name === 'light' ? theme.delta : theme.beta};
+		background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%230d9970%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+      linear-gradient(to bottom, ${({ theme }) => theme.name === 'light' ? theme.delta : theme.alpha} 0%,${({ theme }) => theme.name === 'light' ? theme.epsilon : theme.beta} 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right .7em top 50%, 0 0;
+    background-size: .65em auto, 100%;
+
+    &::-ms-expand {
+    	display: none;
+    }
+
+    &:hover {cursor: pointer;
+    	border-color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+    }
+
+    &:focus {
+    	border-color: ${({ theme }) => theme.eta};
+    }
+
+    & option {
+    	font-weight: 700;
+    }
 	}
 
 `
