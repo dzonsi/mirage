7624025e7d9baeeb1656c3282d71b39cf48e8078
dzonsi <nikola86_v@yahoo.com>
2020-09-05 12:16:08 +0200

    Add new folder animations in style for css animation @keyframes

---
+++ src/style/animations/optionsFadeIn.js
@@ -0,0 +1,6 @@
+import { keyframes } from 'styled-components';
+
+export const optionsFadeIn = keyframes`
+	0% { top: 100%; right: 200%; opacity: 0.3; }
+	100% { top: 0; right: 100%; opacity: 1; }
+`;
