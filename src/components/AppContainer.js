d115318129277f10a0085eea8559a0455bfaec48
dzonsi <nikola86_v@yahoo.com>
2020-10-03 11:37:31 +0200

    Add linear-gradient background to the whole app

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
 	background-color: ${({ theme }) => theme.epsilon};
+	background: linear-gradient(to bottom right, ${({ theme }) => theme.epsilon} 30%, ${({ theme }) => theme.delta});
   min-height: 100vh;
   padding-top: 88px;
 `
