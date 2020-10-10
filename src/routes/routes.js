18d870dd5b8533e46bb28f965012cddd1ff0b598
dzonsi <nikola86_v@yahoo.com>
2020-10-10 18:57:48 +0200

    Add style to TransitionGroup and location to Switch

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,10 +7,15 @@ import {
 	TransitionGroup,
 	CSSTransition
 } from "react-transition-group";
+import styled from 'styled-components';
 
 import { HomeStyled as Home } from '../components/home/Home';
 import { AllUsersStyled as Users } from '../components/users/AllUsers';
 import { SingleUserConnected as User } from '../components/users/SingleUser';
+
+const TransitionGroupStyled = styled(TransitionGroup)`
+	position: relative;
+`;// height of this element is important
 
 const routes = (
 
@@ -20,13 +25,13 @@ const routes = (
 			const { key } = location;
 
 			return (
-				<TransitionGroup>
+				<TransitionGroupStyled>
 					<CSSTransition
 						timeout={300}
 						classNames="page"
 						key={key}
 					>
-						<Switch>
+						<Switch location={location}>
   		  			<Route exact path="/" render={() => <Home />}/>
   		  			<Route path="/users/:id/:resources" render={() => <h2>Resources</h2>}></Route>
   		  			<Route path="/users/:id" render={() => <User />}></Route>
@@ -34,7 +39,7 @@ const routes = (
   		  			<Route path="*" render={() => (<h2>No Match</h2>)}></Route>
   					</Switch>
   				</CSSTransition>
-  			</TransitionGroup>
+  			</TransitionGroupStyled>
 			);
 		}}
 	/>
