3f466a0565d25897bb875e39d715815cf1d2605f
dzonsi <nikola86_v@yahoo.com>
2020-10-10 12:48:00 +0200

    Add react-transition-group components to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,19 +3,42 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import {
+	TransitionGroup,
+	CSSTransition
+} from "react-transition-group";
 
 import { HomeStyled as Home } from '../components/home/Home';
 import { AllUsersStyled as Users } from '../components/users/AllUsers';
 import { SingleUserConnected as User } from '../components/users/SingleUser';
 
 const routes = (
-	<Switch>
-    <Route exact path="/" render={() => <Home />}/>
-    <Route path="/users/:id/:resources" render={() => <h2>Resources</h2>}></Route>
-    <Route path="/users/:id" render={() => <User />}></Route>
-    <Route path="/users" render={() => <Users />}></Route>
-    <Route path="*" render={() => (<h2>No Match</h2>)}></Route>
-  </Switch>
+
+	<Route
+		render={({ location }) => {
+
+			const { key } = location;
+
+			return (
+				<TransitionGroup>
+					<CSSTransition
+						timeout={300}
+						classNames="page"
+						key={key}
+					>
+						<Switch>
+  		  			<Route exact path="/" render={() => <Home />}/>
+  		  			<Route path="/users/:id/:resources" render={() => <h2>Resources</h2>}></Route>
+  		  			<Route path="/users/:id" render={() => <User />}></Route>
+  		  			<Route path="/users" render={() => <Users />}></Route>
+  		  			<Route path="*" render={() => (<h2>No Match</h2>)}></Route>
+  					</Switch>
+  				</CSSTransition>
+  			</TransitionGroup>
+			);
+		}}
+	/>
+
 );
 
 export default routes;
