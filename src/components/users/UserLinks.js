78b1dffb9bad416694c0232d33f3a75b7dbb6bb7
dzonsi <nikola86_v@yahoo.com>
2020-10-17 13:04:16 +0200

    Conpletely change style of component

--- src/components/users/UserLinks.js
+++ src/components/users/UserLinks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink, useParams } from 'react-router-dom';
 import styled from 'styled-components';
-//import { minWidth } from '../../../theme/mixins/minWidth';
+import { minWidth } from '../../theme/mixins/minWidth';
 
 function UserLinks(props) {
 
@@ -24,29 +24,59 @@ export const UserLinksStyled = styled(UserLinks)`
 	justify-content: center;
 	align-items: center;
 
+	${minWidth.xs`
+		margin-top: 50px;
+	`}
+
 	& a {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		width: 100px;
-		height: 100px;
-		color: ${({ theme }) => theme.name === 'light' ? theme.gamma : theme.epsilon };
+		width: 80px;
+		height: 80px;
+		color: ${({ theme }) => theme.name === 'light' ? theme.beta : theme.beta };
 		background-color: ${({ theme }) => theme.beta};
 		font-weight: 700;
 		padding: 0.5rem 1rem;
-		border-radius: 25px;
-		margin: 0 1rem 0 0;
-    background: linear-gradient(-45deg, rgba(0,0,0,0.2), rgba(255,255,255,0.25));
+		border-radius: 50%;
+		margin: 0 0.5rem;
+    background: linear-gradient(-45deg, rgba(0,0,0,0.3), rgba(255,255,255,0.25));
+    box-shadow: none;
+    transition: all .1s linear;
 
-		&:last-child {
-			margin: 0;
-		}
+    ${minWidth.xs`
+			width: 100px;
+			height: 100px;
+		`}
+
+		${minWidth.sm`
+			margin: 0 1rem;
+		`}
+
+		${minWidth.md`
+			font-size: 1.1rem;
+			width: 120px;
+			height: 120px;
+		`}
+
+		${minWidth.lg`
+			font-size: 1.2rem;
+		`}
 
 		&:hover {
-			color: ${({ theme }) => theme.name === 'light' ? theme.gamma : theme.epsilon };
-			box-shadow: ${({ theme }) => theme.name === 'light' ? `2px 2px 8px 0 rgba(0, 0, 0, 0.5),
-			-2px -2px 8px 0 rgba(255, 255, 255, 0.2);` : `2px 2px 8px 0 rgba(0, 0, 0, 0.9),
-			-2px -2px 8px 0 rgba(255, 255, 255, 0.1);` };
+			color: ${({ theme }) => theme.name === 'light' ? theme.beta : theme.beta };
+			box-shadow: ${({ theme }) => theme.name === 'light' ? `4px 4px 16px 0 rgba(0, 0, 0, 0.5),
+			-4px -4px 16px 0 rgba(255, 255, 255, 0.2);` : `4px 4px 16px 0 #000,
+			-4px -4px 16px 0 rgba(255, 255, 255, 0.2);` };
+
+			${minWidth.xs`
+				font-size: 1.3rem;
+			`}
+
+			${minWidth.lg`
+				font-size: 1.4rem;
+			`}
+
 		}
 
 		&:active {
