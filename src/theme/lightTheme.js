6050d57c5cb959e4a37c8e14f4364bb5199cd2d2
dzonsi <nikola86_v@yahoo.com>
2020-11-22 12:31:40 +0100

    Add new colors

--- src/theme/lightTheme.js
+++ src/theme/lightTheme.js
@@ -10,5 +10,9 @@ export const lightTheme = {
 	zeta: '#0d9970',// green
 	eta: '#F64E4D',// orange
 	theta: '#a600c8',// violet-magenta
+	iota: '#00b386',// green-mint
+	kappa: '#fa4c5d',// red-pink
+	lambda: '#5d52fa',// blue-violet
+	mu: '#ff9900',// yellow-gold
 	...breakPoints
 }
