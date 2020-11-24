0e5a7f958a884b5fa153c95fafe6e4f2f71844f3
dzonsi <nikola86_v@yahoo.com>
2020-11-24 21:43:38 +0100

    Change order of colors in SectionNavLinks and style of component

--- src/components/home/sections/HomeLinks.js
+++ src/components/home/sections/HomeLinks.js
@@ -28,10 +28,10 @@ function HomeLinks(props) {
 		return (
 			<div className={props.className}>
 				<div className="links-container">
-					<SectionNavLink to="posts" iconName="pencil-alt" iconPrefix="fas" color="kappa" />
-					<SectionNavLink to="comments" iconName="comments" iconPrefix="fas" color="lambda" />
-					<SectionNavLink to="todos" iconName="check-double" iconPrefix="fas" color="mu" />
-					<SectionNavLink to="photos" iconName="images" iconPrefix="fas" color="iota" />
+					<SectionNavLink to="posts" iconName="pencil-alt" iconPrefix="fas" color="iota" />
+					<SectionNavLink to="comments" iconName="comments" iconPrefix="fas" color="kappa" />
+					<SectionNavLink to="todos" iconName="check-double" iconPrefix="fas" color="lambda" />
+					<SectionNavLink to="photos" iconName="images" iconPrefix="fas" color="mu" />
 				</div>
 			</div>
 		)
@@ -57,9 +57,9 @@ export const HomeLinksStyled = styled(HomeLinksConnected)`
 
 	.links-container {
 		display: flex;
-		flex-direction: column;
-		jusify-content: center;
-		alig-items: center;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
 	}
 
 `
