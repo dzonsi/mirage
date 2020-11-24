af7b5cadcb6439d0d73a7ecb5646d02e2d77d13e
dzonsi <nikola86_v@yahoo.com>
2020-11-24 21:11:43 +0100

    Add new section for home component, HomeLinks

---
+++ src/components/home/sections/HomeLinks.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from '../../../action-creators/usersCreators';
+import styled from 'styled-components';
+
+import { minWidth } from '../../../theme/mixins/minWidth';
+
+import { SectionLoadingStyled as Loading } from '../SectionLoading';
+import { SectionNavLinkStyled as SectionNavLink } from '../SectionNavLink';
+
+function HomeLinks(props) {
+
+	const { loading, users, error } = props;
+
+	useEffect(() => {
+		if(!props.users.length) {
+			props.dispatch(fetchUsers());
+		}
+	}, []);
+
+	if(loading) {
+		return (
+			<Loading />
+		)
+	}
+	if(users.length) {
+
+		return (
+			<div className={props.className}>
+				<div className="links-container">
+					<SectionNavLink to="posts" iconName="pencil-alt" iconPrefix="fas" color="kappa" />
+					<SectionNavLink to="comments" iconName="comments" iconPrefix="fas" color="lambda" />
+					<SectionNavLink to="todos" iconName="check-double" iconPrefix="fas" color="mu" />
+					<SectionNavLink to="photos" iconName="images" iconPrefix="fas" color="iota" />
+				</div>
+			</div>
+		)
+	}
+	if(error) {
+		return (
+			<h2>Something went wrong while fetching users!</h2>
+		)
+	}
+	return null
+
+}
+
+const mapStateToProps = state => ({
+	users: state.usersReducer.users,
+	loading: state.usersReducer.loading,
+	error: state.usersReducer.error
+});
+
+const HomeLinksConnected = connect(mapStateToProps) (HomeLinks);
+
+export const HomeLinksStyled = styled(HomeLinksConnected)`
+
+	.links-container {
+		display: flex;
+		flex-direction: column;
+		jusify-content: center;
+		alig-items: center;
+	}
+
+`
