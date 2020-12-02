d78b1d308ba9d24c61ba2ca8da5d054b442c099a
dzonsi <nikola86_v@yahoo.com>
2020-12-02 20:07:08 +0100

    Add fixed height to div element

--- src/components/home/SectionLoading.js
+++ src/components/home/SectionLoading.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-//import { minWidth } from '../../../theme/mixins/minWidth';
+import { minWidth } from '../../theme/mixins/minWidth';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
 
@@ -14,9 +14,23 @@ function SectionLoading(props) {
 
 export const SectionLoadingStyled = styled(SectionLoading)`
 	width: 100%;
+	height: 209px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	padding: 2rem;
 	color: ${({ theme }) => theme.zeta};
+
+	${minWidth.xs`
+		height: 257px;
+	`}
+
+	${minWidth.sm`
+		height: 277px;
+	`}
+
+	${minWidth.md`
+		height: 349px;
+	`}
+
 `
