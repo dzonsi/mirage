07cf95f74cccb5ebaf2181a4edb097814bfe1ec0
dzonsi <nikola86_v@yahoo.com>
2020-12-02 19:49:27 +0100

    Remove linear gradient background from component

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
 	background-color: ${({ theme }) => theme.epsilon};
-	background: linear-gradient(to bottom right, ${({ theme }) => theme.epsilon} 30%, ${({ theme }) => theme.delta});
   min-height: 100vh;
   padding-top: 88px;
 `
