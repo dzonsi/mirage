6114c8d5691670419a70a1b4ac76beaf571c4bef
dzonsi <nikola86_v@yahoo.com>
2020-12-05 12:20:41 +0100

    Add elements inside component and new style

--- src/components/users/SingleUserOptions.js
+++ src/components/users/SingleUserOptions.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import styled from 'styled-components';
 import { useCloseOutside } from '../../hooks/useCloseOutside';
 
+import { DefaultButton as Button } from '../shared/DefaultButton';
+
 import { optionsFadeIn } from '../../style/animations/optionsFadeIn';
 
 function SingleUserOptions(props) {
@@ -13,7 +15,18 @@ function SingleUserOptions(props) {
 
 	return (
 		<div className={props.className} ref={element}>
-			<h2>Single user options</h2>
+			<div className="button-container">
+				<Button className="edit" padding=".6rem .8rem .6rem">Edit</Button>
+				<Button className="delete" padding=".6rem .8rem .6rem">Delete</Button>
+				<hr />
+				<div>
+					<p>Add new:</p>
+					<Button padding=".6rem .8rem .6rem">Post</Button>
+					<Button padding=".6rem .8rem .6rem">Album</Button>
+					<Button padding=".6rem .8rem .6rem">Todo</Button>
+					<Button padding=".6rem .8rem .6rem">Image</Button>
+				</div>
+			</div>
 		</div>
 	)
 
@@ -23,7 +36,55 @@ export const SingleUserOptionsStyled = styled(SingleUserOptions)`
 	position: absolute;
 	top: 0;
 	right: 100%;
-	background-color: ${({ theme }) => theme.gamma};
+	font-size: 1rem;
+	width: 200px;
+	text-align: center;
+	color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+	background-color: ${({ theme }) => theme.name === 'light' ? theme.delta : theme.gamma};
+	border-radius: 20px;
+	padding: 1rem;
 	animation: ${optionsFadeIn} .2s forwards;
 	z-index: 9;
+
+	& div.button-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+
+		& button {
+			font-weight: 700;
+			border-radius: 0.5rem;
+			margin: 0 0 0.5rem;
+			opacity: 0.7;
+			width: 100%;
+
+			&.edit {
+				color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+				border: 1px solid ${({ theme }) => theme.zeta};
+
+				&:hover {
+					color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+				}
+			}
+
+			&.delete {
+				color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+				border: 1px solid ${({ theme }) => theme.eta};
+
+				&:hover {
+					color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+				}
+
+			}
+
+			&:hover {
+				color: ${({ theme }) => theme.name === 'light' ? theme.alpha : theme.epsilon};
+				opacity: 1;
+			}
+
+		}
+
+	}
+
 `
