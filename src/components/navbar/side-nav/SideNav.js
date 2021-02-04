18e6cf8ac25a44453e90e04effd1b31983f626de
unknown <nikola86_v@yahoo.com>
2021-02-04 12:57:40 +0100

    Add new triangle decoration and change style of whole component

--- src/components/navbar/side-nav/SideNav.js
+++ src/components/navbar/side-nav/SideNav.js
@@ -34,6 +34,13 @@ function SideNav(props) {
 			</Button>
 			<div className="round-decoration"></div>
 			<div className="square-decoration"></div>
+			<div className="triange-decoration">
+				<div className="triangle-container">
+					<div className="side bottom"></div>
+					<div className="side left"></div>
+					<div className="side right"></div>
+				</div>
+			</div>
 		</div>
 	)
 }
@@ -87,15 +94,21 @@ const SideNavStyled = styled(SideNav)`
 			flex-direction: column;
 			justify-content: center;
 			align-items: center;
+
+			${
+				minWidth.sm`
+					align-items: flex-start;
+				`
+			}
 		}
 
 		& a {
 			font-size: 20px;
 			font-weight: 700;
 			color: ${({ theme }) => theme.beta};
-			padding: 0.25rem 0.5rem;
+			padding: 0 0.5rem;
 			margin-bottom: 0.5rem;
-			transition: font-size .1s linear;
+			transition: color .1s linear, font-size .1s linear;
 
 			&.active {
 				color: ${({ theme }) => theme.eta};
@@ -114,6 +127,7 @@ const SideNavStyled = styled(SideNav)`
 			${
 				minWidth.lg`
 					font-size: 30px;
+					padding: 0 0.5rem;
 				`
 			}
 
@@ -128,6 +142,8 @@ const SideNavStyled = styled(SideNav)`
 		position: absolute;
 		top: 1.5rem;
 		right: 1.5rem;
+		transition: all .1s linear;
+		z-index: 14;
 
 		&:hover {
 			color: ${({ theme }) => theme.zeta};
@@ -138,21 +154,15 @@ const SideNavStyled = styled(SideNav)`
 		}
 
 		${
-			minWidth.sm`
-				font-size: 28px;
-				padding: 0 0.7rem;
-				top: 1rem;
-				right: 1rem;
-
-				&:active {
-					padding: 0 0.7rem;
-				}
+			minWidth.xs`
+				top: 2rem;
+				right: 2rem;
 			`
 		}
 
 		${
-			minWidth.lg`
-				font-size: 38px;
+			minWidth.sm`
+				font-size: 2.5rem;
 				padding: 0 1rem;
 				top: 2rem;
 				right: 2rem;
@@ -160,6 +170,26 @@ const SideNavStyled = styled(SideNav)`
 				&:active {
 					padding: 0 1rem;
 				}
+			`
+		}
+
+		${
+			minWidth.md`
+				font-size: 3rem;
+				padding: 0 1.2rem;
+				top: 3rem;
+				right: 3rem;
+
+				&:active {
+					padding: 0 1.2rem;
+				}
+			`
+		}
+
+		${
+			minWidth.lg`
+				top: 4rem;
+				right: 8rem;
 			`
 		}
 
@@ -174,6 +204,18 @@ const SideNavStyled = styled(SideNav)`
 		position: absolute;
 		bottom: -50px;
 		right: -50px;
+		transition: all .1s linear;
+		z-index: 13;
+
+		${
+			minWidth.md`
+				width: 300px;
+				height: 300px;
+				border-width: 40px;
+				bottom: 90px;
+				right: 70px;
+			`
+		}
 	}
 
 	.square-decoration {
@@ -186,6 +228,67 @@ const SideNavStyled = styled(SideNav)`
 		position: absolute;
 		top: -50px;
 		left: -50px;
+		transition: all .1s linear;
+		z-index: 13;
+
+		${
+			minWidth.md`
+				width: 300px;
+				height: 300px;
+				border-width: 40px;
+				top: 20px;
+				left: 20px;
+				transform: rotate(35deg);
+			`
+		}
+	}
+
+	.triange-decoration {
+		position: absolute;
+		bottom: 0;
+		left: -130px;
+		transition: all .1s linear;
+		opacity: 0.2;
+		transform: rotate(15deg);
+		z-index: 13;
+
+		& .triangle-container {
+			position: relative;
+		}
+
+		& .side {
+			width: 250px;
+			height: 40px;
+			background-color: ${({ theme }) => theme.lambda};
+			border-radius: 30px;
+			position: absolute;
+			top: 0;
+			bottom: 0;
+		}
+
+		& .left {
+			transform: rotate(60deg);
+			transform-origin: right center;
+			top: 15px;
+			left: -10px;
+		}
+
+		& .right {
+			transform: rotate(-60deg);
+			transform-origin: left center;
+			top: 15px;
+			left: 10px;
+		}
+
+		${
+			minWidth.md`
+				bottom: 150px;
+				left: 220px;
+				transform: rotate(35deg);
+				transition: all .1s linear;
+			`
+		}
+
 	}
 `
 
