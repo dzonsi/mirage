0f9a51f9c9b645c680fa2d37ae56a0de3db622c0
unknown <nikola86_v@yahoo.com>
2021-02-09 15:26:27 +0100

    Import Loading and Post components and conditionally return html from component

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -6,22 +6,46 @@ import { fetchPosts } from '../../action-creators/postsCreators';
 import PropTypes from 'prop-types';
 import { minWidth } from '../../theme/mixins/minWidth';
 
+import { LoadingStyled as Loading } from '../shared/Loading';
+import { PostStyled as Post } from './Post';
+
 function Posts(props) {
 
 	const { posts, loading, error } = props;
 
 	useEffect(() => {
-		if(!props.posts.length) {
+		if(!posts.length) {
 			props.fetchPosts();
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return (
-		<div className={props.className}>
-			<h2>Posts</h2>
-		</div>
-	)
+	if(loading) {
+		return (
+			<Loading />
+		)
+	}
+
+	if(posts.length) {
+		return (
+			<section id="posts" className={props.className}>
+				<h2>Posts</h2>
+				{posts.map(post => <Post key={post.id} post={post} />)}
+			</section>
+		)
+	}
+
+	if(error) {
+		return (
+			<section id="posts" className={props.className}>
+				<h2>Posts</h2>
+				<p>Something went wrong while fetching posts!</p>
+				<p>Please, try again later...</p>
+			</section>
+		)
+	}
+
+	return null;
 }
 
 Posts.propTypes = {
