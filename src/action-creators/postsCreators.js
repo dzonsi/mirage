5c291b91bf4a5bc49804531e76570d54a8e1a48a
unknown <nikola86_v@yahoo.com>
2021-02-09 13:21:34 +0100

    Add new action creator file for posts, postsCreators.js

---
+++ src/action-creators/postsCreators.js
@@ -0,0 +1,36 @@
+import { FETCH_POSTS_BEGIN } from '../actions/actionTypes';
+import { FETCH_POSTS_SUCCESS } from '../actions/actionTypes';
+import { FETCH_POSTS_ERROR } from '../actions/actionTypes';
+
+import axios from 'axios';
+
+// fetch posts
+
+export const fetchPostsBegin = () => ({
+	type: FETCH_POSTS_BEGIN
+});
+
+export const fetchPostsSuccess = posts => ({
+	type: FETCH_POSTS_SUCCESS,
+	payload: posts
+});
+
+export const fetchPostsError = error => ({
+	type: FETCH_POSTS_ERROR,
+	payload: error
+});
+
+export function fetchPosts() {
+	return function(dispatch) {
+		dispatch(fetchPostsBegin());
+		return setTimeout(() => {
+			axios.get('https://jsonplaceholder.typicode.com/posts')
+			.then( response => {
+				dispatch(fetchPostsSuccess(response.data));
+			})
+			.catch(error => {
+				dispatch(fetchPostsError(error));
+			})
+		}, 500);
+	}
+}
