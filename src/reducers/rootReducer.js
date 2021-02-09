004733f22a5af4c551444ce74b7d3daf815217f4
unknown <nikola86_v@yahoo.com>
2021-02-09 13:32:13 +0100

    Import postsReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import navbarReducer from './navbarReducer';
 import usersReducer from './usersReducer';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
 	navbarReducer,
-	usersReducer
+	usersReducer,
+	postsReducer
 });
