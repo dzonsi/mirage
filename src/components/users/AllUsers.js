7c98d3468eb9eb714692ae44a4df7b7fc26edf4f
unknown <nikola86_v@yahoo.com>
2021-02-10 17:29:01 +0100

    Connect component to Redux and pass methods to Status component

--- src/components/users/AllUsers.js
+++ src/components/users/AllUsers.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 import styled from 'styled-components';
 
-import { StatusConnectedStyled as Status } from '../shared/Status';
+import { toggleUsersOptions } from '../../action-creators/usersCreators';
+
+import { StatusStyled as Status } from '../shared/Status';
 import { AllUsersOptionsStyled as Options } from './AllUsersOptions';
 import { AllUsersBodyStyled as AllUsersBody } from './AllUsersBody';
 
 function AllUsers(props) {
+
+	const {
+		showOptions,
+		toggleUsersOptions
+	} = props;
 
 	return (
 		<section className={props.className} id="all-users">
 			<Status
 				type="Users"
 				options={<Options />}
+				showOptions={showOptions}
+				toggleUsersOptions={toggleUsersOptions}
 			/>
 			<AllUsersBody />
 		</section>
@@ -19,6 +29,16 @@ function AllUsers(props) {
 
 }
 
-export const AllUsersStyled = styled(AllUsers)`
+const mapStateToProps = state => ({
+	showOptions: state.usersReducer.usersOptions
+});
+
+const mapDispatchToProps = {
+	toggleUsersOptions
+}
+
+const AllUsersConnected = connect(mapStateToProps, mapDispatchToProps) (AllUsers);
+
+export const AllUsersStyled = styled(AllUsersConnected)`
 
 `
