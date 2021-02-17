3690a1b3c64ea75b2ae7a4c8b6f879546e560fae
unknown <nikola86_v@yahoo.com>
2021-02-17 19:06:11 +0100

    Change html and style of component

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -3,14 +3,28 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { minWidth } from '../../theme/mixins/minWidth';
+import { importAll, getRandom } from '../../functions/functions';
+
+// import all images as object
+const images = importAll(require.context('../../assets/images/avatar', false, /\.png/));
+
+// array from all images keys
+const imgArray = Object.keys(images);
 
 function Post(props) {
 
 	const { post, user } = props;
+	const num = getRandom(0, imgArray.length - 1);
 
 	return (
 		<article className={props.className}>
-			<NavLink to={`/users/${post.userId}`} className="user">{user.name}</NavLink>
+			<div className="post-heading">
+				<img className="user-img" src={images[imgArray[num]]} alt="User profile" />
+				<NavLink to={`/users/${post.userId}`} className="user">
+					<span className="name">{user.name}</span>&nbsp;
+					<span className="username">@{user.username}</span>
+				</NavLink>
+			</div>
 			<h3 className="title">{post.title}</h3>
 			<p className="body">{post.body}</p>
 		</article>
@@ -33,18 +47,41 @@ export const PostStyled = styled(Post)`
 		margin-bottom: 0;
 	}
 
-	& .user, & .title, & .body {
+	& .post-heading {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		margin-bottom: 0.5rem;
+	}
+
+	& .user-img {
+		width: 30px;
+		height: 30px;
+		border-radius: 50%;
+	}
+
+	& .title, & .body {
 		font-size: 0.8rem;
 		margin-bottom: 0.5rem;
 	}
 
-	.user {
+	& .user {
 		display: inline-block;
+		font-size: 0.8rem;
+		margin-left: 10px;
+	}
+
+	& .name {
 		font-size: 1rem;
 		font-weight: 700;
 	}
 
-	.title {
+	& .username {
+		color: ${({ theme }) => theme.beta};
+	}
+
+	& .title {
 		color: ${({ theme }) => theme.zeta};
 	}
 `
