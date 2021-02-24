043a10098e3d945d45192e3f7ff65c3b83401905
unknown <nikola86_v@yahoo.com>
2021-02-24 20:52:05 +0100

    Create two new functions for manage focus in side nav

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -12,3 +12,21 @@ export const importAll = r => {
 
 // math random between to numbers
 export const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) ) + min;
+
+// move focus to top element in list when bottom element tab key is pressed
+// top is react ref object
+export const moveFocusToTop = top => e => {
+  if (e.key === 'Tab' && !e.shiftKey) {
+    e.preventDefault();
+    top.current.focus();
+  }
+}
+
+// move focus to bottom element in list when top element tab key is pressed together with shift key
+// bottom is react ref object
+export const moveFocusToBottom = bottom => e => {
+  if (e.key === 'Tab' && e.shiftKey) {
+    e.preventDefault();
+    bottom.current.focus();
+  }
+}
