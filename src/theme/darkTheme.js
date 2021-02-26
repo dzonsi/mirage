9e22b005169b193722fc7fbf2170ca5bda315f62
unknown <nikola86_v@yahoo.com>
2021-02-26 13:23:41 +0100

    Change iota color to yellow, #febe30

--- src/theme/darkTheme.js
+++ src/theme/darkTheme.js
@@ -10,7 +10,7 @@ export const darkTheme = {
 	zeta: '#0d9970',// green
 	eta: '#F64E4D',// orange
 	theta: '#a600c8',// violet-magenta
-	iota: '#00b386',// green-mint
+	iota: '#febe30',// yellow
 	kappa: '#5d52fa',// violet
 	lambda: '#fa4c5d',// red-pink
 	mu: '#3071f3',// blue
