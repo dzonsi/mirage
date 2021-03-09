5f5b28498f2a12c646671b74550e153463684487
unknown <nikola86_v@yahoo.com>
2021-03-09 13:04:18 +0100

    Pass children prop with description to SectionNavLink components

--- src/components/home/sections/HomeLinks.js
+++ src/components/home/sections/HomeLinks.js
@@ -12,10 +12,10 @@ function HomeLinks(props) {
 			<div className={props.className}>
 				<Heading to="resources" />
 				<div className="links-container">
-					<SectionNavLink to="posts" iconName="pencil-alt" iconPrefix="fas" color="iota" />
-					<SectionNavLink to="comments" iconName="comments" iconPrefix="fas" color="kappa" />
-					<SectionNavLink to="todos" iconName="check-double" iconPrefix="fas" color="lambda" />
-					<SectionNavLink to="photos" iconName="images" iconPrefix="fas" color="mu" />
+					<SectionNavLink to="posts" iconName="pencil-alt" iconPrefix="fas" color="iota">A piece of writing, image, or other item of content published online, typically on a blog or social media website or application.</SectionNavLink>
+					<SectionNavLink to="comments" iconName="comments" iconPrefix="fas" color="kappa">A verbal or written remark expressing an opinion or reaction.</SectionNavLink>
+					<SectionNavLink to="todos" iconName="check-double" iconPrefix="fas" color="lambda">A task yet to be done or an item on a to-do list.</SectionNavLink>
+					<SectionNavLink to="photos" iconName="images" iconPrefix="fas" color="mu">The process or art of producing images of objects on sensitized surfaces by the chemical action of light.</SectionNavLink>
 				</div>
 			</div>
 		)
